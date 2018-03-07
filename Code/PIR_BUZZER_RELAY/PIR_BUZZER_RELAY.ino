c4c3a5f9df947cb296f97d7a4dda0d332f1d65d8
MakramChrahil <37145495+MakramChrahil@users.noreply.github.com>
2018-03-07 14:44:03 +0000

    Add files via upload

---
+++ Code/PIR_BUZZER_RELAY/PIR_BUZZER_RELAY.ino
@@ -0,0 +1,27 @@
+#define RELAY 11
+#define BUZZER 10
+#define PIR 9
+
+int val_pir=0;
+
+void setup() {
+  pinMode(BUZZER, OUTPUT); 
+  pinMode(RELAY,OUTPUT );     
+  pinMode(PIR, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  val_pir=digitalRead(PIR);
+  if( val_pir==HIGH) {
+    Serial.println("Mouvement detecté");
+    digitalWrite(BUZZER,HIGH);
+    digitalWrite(RELAY,LOW);    //commande en inverse
+    }
+   else  { 
+    Serial.println("Aucun mouvement ");
+    digitalWrite(BUZZER,LOW);
+    digitalWrite(RELAY,HIGH);
+    }
+delay(300);
+}
